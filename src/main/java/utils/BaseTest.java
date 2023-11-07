a8608ac9e35169c32f215c865159232e44a0805b
Dragos Tanta <dragos.tanta@gmail.com>
2023-11-07 21:28:46 +0200

    comit initial

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+	public static WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();//maximizeaza fereastra de browser
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://keybooks.ro");
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(4000);
+		//driver.close();//inchide tabul curent
+		driver.quit(); //inchide instanta de browser
+		
+	}
+	
+	
+	
+}
---
+++ src/main/java/utils/SeleniumWrappers.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumWrappers extends BaseTest{
+	
+	
+	public void click(By locator) {
+		//WebElement element = driver.findElement(locator);
+		waitForElementToBeVisible(locator);
+		returnElement(locator).click();
+		
+	}
+	
+	
+	public void sendKeys(By locator, String text) {
+		//WebElement element = driver.findElement(locator);
+		waitForElementToBeVisible(locator);
+		returnElement(locator).sendKeys(text);
+	}
+	
+	
+	public WebElement returnElement(By locator) {
+		return driver.findElement(locator);
+	}
+	
+	public void waitForElementToBeVisible(By locator) {
+		WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	
+	}
+
+}
