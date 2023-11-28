f3ee79bc32d65afc1ec3ccfb3de346447169e691
Dragos Tanta <dragos.tanta@gmail.com>
2023-11-28 20:40:00 +0200

    curs24 intermediar

---
+++ src/main/java/pages/ContactsPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import utils.SeleniumWrappers;
+
+public class ContactsPage extends SeleniumWrappers{
+
+	
+	public By nameField = By.cssSelector("input[name='your-name']");
+	
+	
+}
